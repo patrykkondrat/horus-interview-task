95a5ae7a3a1b3d8d6d320cd24732e81900823f5f
patrykkondrat <01150244@pw.edu.pl>
2023-02-26 14:23:13 +0100

    impl count method

--- src/main/java/org/horus/interview/Wall.java
+++ src/main/java/org/horus/interview/Wall.java
@@ -1,7 +1,32 @@
 package org.horus.interview;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Wall implements Structure {
     private List<Block> blocks;
+
+    @Override
+    public Optional<Block> findBlockByColor(String color) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Block> findBlocksByMaterial(String material) {
+        return null;
+    }
+
+    @Override
+    public int count() {
+        int count = 0;
+        for (Block block : blocks) {
+            if (block instanceof CompositeBlock) {
+                count += ((CompositeBlock) block).getBlocks().size();
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
 }
